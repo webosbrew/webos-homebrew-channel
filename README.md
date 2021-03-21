94f9748b33a5157311137322518a48212de688b3
David Buchanan <d@vidbuchanan.co.uk>
2021-03-21 07:17:09 +0000

    Add 'doas' binary

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # webos-homebrew-channel
+
+Manual installation:
+
+```
+# luna-send -i -f luna://com.webos.appInstallService/installLocal '{"id": "tv.rootmy.hbchannel", "subscribe": true, "ipkUrl": "/home/root/tv.rootmy.hbchannel_0.0.1_all.ipk", "target": {"deviceId":"INTERNAL_STORAGE", "share": true}}'
+```
---
+++ tools/doas/doas.c
@@ -0,0 +1,35 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+	if (argc < 4) {
+		printf("USAGE: %s uid gid command\n", argv[0]);
+		return -1;
+	}
+	
+	uid_t uid = atoi(argv[1]);
+	uid_t gid = atoi(argv[2]);
+	
+	if (setresuid(uid, uid, uid) != 0) {
+		perror("doas: setresuid");
+		return -1;
+	}
+	
+	if (setresgid(gid, gid, gid) != 0) {
+		perror("doas: setresgid");
+		return -1;
+	}
+	
+	// Break out of jail, if we're in one
+	if (chroot("/proc/1/root/") != 0) {
+		perror("doas: chroot");
+		return -1;
+	}
+	
+	execve(argv[3], &argv[3], envp);
+	perror("execve"); // execve only returns on error
+	return -1;
+}
