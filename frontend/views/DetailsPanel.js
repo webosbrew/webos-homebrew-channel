c24595a1f5754e942c1d8f5bdbc9b62409b19bb7
rhinoswirl <gaymerdolphin@gmail.com>
2021-04-23 23:57:34 +0100

    Added app description to DetailsPanel

--- frontend/views/DetailsPanel.js
+++ frontend/views/DetailsPanel.js
@@ -95,7 +95,11 @@ module.exports = kind({
         {
           components: [
             {kind: Divider, content: 'Description'},
-            {kind: BodyText, content: 'Some long-ish project description......'},
+            {
+              name: 'appDescription',
+              kind: BodyText,
+              content: 'No description provided for this package'
+            },
             {
               kind: LabeledTextItem,
               label: 'Project page',
@@ -144,6 +148,15 @@ module.exports = kind({
     // This is data loaded from the web service
     {from: 'packageInfo.title', to: 'title'},
     {from: 'packageInfo.id', to: 'titleBelow'},
+    {
+      from: 'packageInfo.appDescription', to: '$.appDescription.content', transform: function (description) {
+        var appDescription = 'No description provided for this package'
+        if (description instanceof String && description.trim().length) {
+          appDescription = description;
+        }
+        return appDescription;
+      }
+    },
     {from: 'packageInfo.version', to: '$.version.text'},
     {from: 'packageInfo.sourceUrl', to: '$.projectPage.text'},
     {
