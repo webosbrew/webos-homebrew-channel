9d65a738162e42bfd1e3422441766503dcae3ab6
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-05-03 22:44:58 +0900

    Added docs for webos-service-remote.Service

--- services/webos-service-remote/service.js
+++ services/webos-service-remote/service.js
@@ -2,16 +2,22 @@ import Message from './message';
 import Handle from './sshbus';
 import Subscription from './subscription';
 
+/**
+ * Drop in replacement for webos-service, but using SSH shell to call luna-send-pub instead.
+ */
 export default class Service {
+    /**
+     * 
+     * @param {Service} service from webos-service module. Used for retreving SSH pubkey passphrase
+     */
     constructor(service) {
         this.sendingHandle = new Handle(service);
     }
-    subscribe(uri, args) {
-        if (typeof args !== "object" || args === null) {
-            throw ("args must be an object");
-        }
-        return new Subscription(this.sendingHandle, uri, args);
-    }
+
+    /* Call a service on the bus
+     * The args parameter is a JSON-compatible object
+     * The callback gets passed a Message object
+     */
     call(uri, args, callback) {
         if (typeof args !== "object" || args === null) {
             throw ("args must be an object");
@@ -27,5 +33,16 @@ export default class Service {
                 returnValue: false, errorCode: -1, errorText: String(error)
             })), handle));
         });
+    }
+
+    /* Subscribe to a service on the bus
+     * The args parameter is a JSON-compatible object
+     * Returns a Subscription object which raises events when responses come in
+     */
+    subscribe(uri, args) {
+        if (typeof args !== "object" || args === null) {
+            throw ("args must be an object");
+        }
+        return new Subscription(this.sendingHandle, uri, args);
     }
 }
