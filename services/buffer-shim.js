744b622848e53cd2729540fe9c87689abfeaab63
Piotr Dobrowolski <admin@tastycode.pl>
2021-05-06 10:11:15 +0200

    eslint/prettier configuration
    We use eslint + prettier config based on airbnb codestyle
    (https://github.com/airbnb/javascript) with some minor adjustments:
    
    * allowed console statements
    * allowed restricted syntax (for..in / for..of)
    * extended line width to 140 characters, as this seemed to be our main
    target so far
    * ignored whole frontend directory (will reenable that after migrating
    from enyo-dev to webpack + babel)
    
    A bunch of strategic local rule disables has been introduced to not
    rewrite too much code in a single commit - this needs to be reviewed and
    cleaned up later.
    
    This also incidentally revealed a bug which broke hbchannel
    reelevation...

--- services/buffer-shim.js
+++ services/buffer-shim.js
@@ -1,21 +1,23 @@
+/* eslint-disable no-buffer-constructor */
+const { SlowBuffer } = require('buffer');
+
 function newBuffer(data, encoding, len) {
-    return new Buffer(data, encoding, len);
+  return new Buffer(data, encoding, len);
 }
 
 function newSlowBuffer(data, encoding, len) {
-    var SlowBuffer = require('buffer').SlowBuffer;
-    return new SlowBuffer(data, encoding, len);
+  return new SlowBuffer(data, encoding, len);
 }
 
 if (!Buffer.alloc) {
-    Buffer.alloc = newBuffer;
+  Buffer.alloc = newBuffer;
 }
 if (!Buffer.allocUnsafe) {
-    Buffer.allocUnsafe = newBuffer;
+  Buffer.allocUnsafe = newBuffer;
 }
 if (!Buffer.allocUnsafeSlow) {
-    Buffer.allocUnsafeSlow = newSlowBuffer;
+  Buffer.allocUnsafeSlow = newSlowBuffer;
 }
 if (!Buffer.from) {
-    Buffer.from = newBuffer;
+  Buffer.from = newBuffer;
 }
--- services/service.js
+++ services/service.js
@@ -1,36 +1,35 @@
-import "regenerator-runtime/runtime";
-import "es6-shim";
+import 'regenerator-runtime/runtime';
+import 'es6-shim';
 import './buffer-shim';
 
-import serviceInfo from './services.json';
 import Service from 'webos-service';
-import child_process from 'child_process';
+import { exec, spawn } from 'child_process';
 import path from 'path';
 
-import * as Promise from "bluebird";
+import * as Promise from 'bluebird';
 import fs from 'fs';
 import pipeline from 'stream.pipeline-shim';
 import fetch from 'node-fetch';
 import progress from 'progress-stream';
 import { createHash } from 'crypto';
+import serviceInfo from './services.json';
 
 import ServiceRemote from './webos-service-remote';
 
 fetch.Promise = Promise;
 const pipelinePromise = Promise.promisify(pipeline);
-const execPromise = Promise.promisify(child_process.exec);
+const execPromise = Promise.promisify(exec);
 const unlinkPromise = Promise.promisify(fs.unlink);
 const writeFilePromise = Promise.promisify(fs.writeFile);
 
-var service = new Service(serviceInfo.id);
-var serviceRemote = new ServiceRemote(service);
+const service = new Service(serviceInfo.id);
+const serviceRemote = new ServiceRemote(service);
 
 function installerService() {
   if (process.getuid() === 0) {
     return service;
-  } else {
-    return serviceRemote;
   }
+  return serviceRemote;
 }
 
 function promiseCall(svc, uri, args) {
@@ -42,12 +41,15 @@ function promiseCall(svc, uri, args) {
       } else {
         reject(payload);
       }
-    })
+    });
   });
 }
 
 function createToast(message) {
-  return promiseCall(service, 'luna://com.webos.notification/createToast', { sourceId: pkgInfo.name, message });
+  return promiseCall(service, 'luna://com.webos.notification/createToast', {
+    sourceId: serviceInfo.id,
+    message,
+  });
 }
 
 /**
@@ -70,7 +72,7 @@ async function installPackage(filePath) {
   return new Promise((resolve, reject) => {
     const svc = installerService();
     const req = svc.subscribe('luna://com.webos.appInstallService/dev/install', {
-      id: "testing",
+      id: 'testing',
       ipkUrl: filePath,
       subscribe: true,
     });
@@ -93,78 +95,85 @@ async function installPackage(filePath) {
   });
 }
 
-service.register("install", async (message) => {
-  const targetPath = `/tmp/.hbchannel-incoming-${Date.now()}.ipk`;
+service.register(
+  'install',
+  async (message) => {
+    const targetPath = `/tmp/.hbchannel-incoming-${Date.now()}.ipk`;
 
-  try {
-    message.respond({
-      statusText: 'downloading',
-    });
+    try {
+      message.respond({
+        statusText: 'downloading',
+      });
 
-    // Download file
-    const res = await fetch(message.payload.ipkUrl);
-    if (!res.ok) {
-      throw new Error(res.statusText);
-    }
-    const progressReporter = progress({
-      length: res.headers.get('content-length'),
-      time: 300 /* ms */
-    });
-    progressReporter.on('progress', (progress) => {
-      message.respond({ statusText: 'downloading', progress: progress.percentage });
-    });
-    const targetFile = fs.createWriteStream(targetPath);
-    await pipelinePromise(res.body, progressReporter, targetFile);
+      // Download file
+      const res = await fetch(message.payload.ipkUrl);
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      const progressReporter = progress({
+        length: res.headers.get('content-length'),
+        time: 300 /* ms */,
+      });
+      progressReporter.on('progress', ({ percentage }) => {
+        message.respond({
+          statusText: 'downloading',
+          progress: percentage,
+        });
+      });
+      const targetFile = fs.createWriteStream(targetPath);
+      await pipelinePromise(res.body, progressReporter, targetFile);
 
-    message.respond({
-      statusText: 'verifying',
-    });
+      message.respond({
+        statusText: 'verifying',
+      });
 
-    const checksum = await hashFile(targetPath, 'sha256');
-    if (checksum !== message.payload.ipkHash) {
-      throw new Error('Invalid file checksum');
-    }
-
-    message.respond({
-      statusText: 'installing',
-    });
-
-    const installedPackageId = await installPackage(targetPath);
-
-    message.respond({
-      statusText: 'finished',
-      finished: true,
-      returnValue: true
-    });
-
-    await createToast(`Application installed: ${installedPackageId}`);
-
-    if (installedPackageId === 'org.webosbrew.hbchannel') {
-      if (process.getuid() === 0) {
-        console.info('Elevating service...');
-        await execPromise(path.join(__dirname, 'elevate-service'));
-        console.info('Finished, dying as soon as possible.');
+      const checksum = await hashFile(targetPath, 'sha256');
+      if (checksum !== message.payload.ipkHash) {
+        throw new Error('Invalid file checksum');
       }
 
-      service.activityManager.idleTimeout = 1;
-      await createToast('Homebrew Channel update finished');
-    }
-  } catch (err) {
-    console.error(err);
-    console.log(err.stack);
-    message.respond({
-      returnValue: false,
-      errorText: err.toString(),
-    });
-  } finally {
-    fs.unlink(targetPath, (err) => console.warn('Unable to remove file!', err));
-  }
+      message.respond({
+        statusText: 'installing',
+      });
 
-  message.cancel();
-}, () => {
-  console.info('canceled!');
-  // TODO
-});
+      const installedPackageId = await installPackage(targetPath);
+
+      message.respond({
+        statusText: 'finished',
+        finished: true,
+        returnValue: true,
+      });
+
+      await createToast(`Application installed: ${installedPackageId}`);
+
+      if (installedPackageId === 'org.webosbrew.hbchannel') {
+        if (process.getuid() === 0) {
+          console.info('Elevating service...');
+          await execPromise(path.join(__dirname, 'elevate-service'));
+          console.info('Finished, dying as soon as possible.');
+        }
+
+        service.activityManager.idleTimeout = 1;
+        await createToast('Homebrew Channel update finished');
+      }
+    } catch (err) {
+      console.error(err);
+      console.log(err.stack);
+      message.respond({
+        returnValue: false,
+        errorText: err.toString(),
+      });
+    } finally {
+      fs.unlink(targetPath, (err) => console.warn('Unable to remove file!', err));
+    }
+
+    message.cancel();
+  },
+  () => {
+    console.info('canceled!');
+    // TODO
+  },
+);
 
 async function flagRead(name) {
   return fs.existsSync(`/var/luna/preferences/${name}`);
@@ -172,19 +181,17 @@ async function flagRead(name) {
 
 async function flagSet(name, value) {
   if (value === true) {
-    if (!await flagRead(name)) {
+    if (!(await flagRead(name))) {
       await writeFilePromise(`/var/luna/preferences/${name}`, '1');
     }
-  } else {
-    if (await flagRead(name)) {
-      await unlinkPromise(`/var/luna/preferences/${name}`);
-    }
+  } else if (await flagRead(name)) {
+    await unlinkPromise(`/var/luna/preferences/${name}`);
   }
 
   return flagRead(name);
 }
 
-service.register("getConfiguration", async (message) => {
+service.register('getConfiguration', async (message) => {
   message.respond({
     returnValue: true,
     root: process.getuid() === 0,
@@ -195,7 +202,7 @@ service.register("getConfiguration", async (message) => {
   });
 });
 
-service.register("setConfiguration", async (message) => {
+service.register('setConfiguration', async (message) => {
   try {
     const resp = {};
     if (message.payload.telnetDisabled !== undefined) {
@@ -212,7 +219,7 @@ service.register("setConfiguration", async (message) => {
     }
     message.respond({
       returnValue: true,
-      ...resp
+      ...resp,
     });
   } catch (err) {
     message.respond({
@@ -222,7 +229,7 @@ service.register("setConfiguration", async (message) => {
   }
 });
 
-service.register("reboot", async (message) => {
+service.register('reboot', async (message) => {
   try {
     await execPromise('reboot');
     message.respond({
@@ -236,7 +243,7 @@ service.register("reboot", async (message) => {
   }
 });
 
-service.register("checkRoot", function (message) {
+service.register('checkRoot', (message) => {
   message.respond({
     returnValue: process.getuid() === 0,
   });
@@ -244,7 +251,7 @@ service.register("checkRoot", function (message) {
 
 // This roughly replicates com.webos.applicationManager/getAppInfo request in an
 // environment-independent way (non-root vs root)
-service.register("getAppInfo", async (message) => {
+service.register('getAppInfo', async (message) => {
   try {
     const appId = message.payload.id;
 
@@ -254,7 +261,7 @@ service.register("getAppInfo", async (message) => {
 
     const svc = installerService();
     const appList = await promiseCall(svc, 'luna://com.webos.applicationManager/dev/listApps', {});
-    const appInfo = appList.apps.find(a => a.id === appId);
+    const appInfo = appList.apps.find((a) => a.id === appId);
     if (appInfo) {
       message.respond({
         returnValue: true,
@@ -269,62 +276,62 @@ service.register("getAppInfo", async (message) => {
     message.respond({
       returnValue: false,
       errorText: err.toString(),
-    })
+    });
   }
   message.cancel();
 });
 
-service.register("exec", function (message) {
-  child_process.exec(message.payload.command, { encoding: "buffer" }, function (error, stdout, stderr) {
+service.register('exec', (message) => {
+  exec(message.payload.command, { encoding: 'buffer' }, (error, stdout, stderr) => {
     message.respond({
       returnValue: !error,
-      error: error,
+      error,
       stdoutString: stdout.toString(),
-      stdoutBytes: stdout.toString("base64"),
+      stdoutBytes: stdout.toString('base64'),
       stderrString: stderr.toString(),
-      stderrBytes: stderr.toString("base64")
+      stderrBytes: stderr.toString('base64'),
     });
   });
 });
 
-service.register("spawn", function (message) {
-  var proc = child_process.spawn("/bin/sh", ["-c", message.payload.command]);
+service.register('spawn', (message) => {
+  const proc = spawn('/bin/sh', ['-c', message.payload.command]);
 
-  proc.stdout.on('data', function (data) {
+  proc.stdout.on('data', (data) => {
     message.respond({
-      event: "stdoutData",
+      event: 'stdoutData',
       stdoutString: data.toString(),
-      stdoutBytes: data.toString("base64")
+      stdoutBytes: data.toString('base64'),
     });
   });
-  proc.stderr.on('data', function (data) {
+  proc.stderr.on('data', (data) => {
     message.respond({
-      event: "stderrData",
+      event: 'stderrData',
       stderrString: data.toString(),
-      stderrBytes: data.toString("base64")
+      stderrBytes: data.toString('base64'),
     });
   });
-  proc.on('close', function (code) {
+  proc.on('close', (code) => {
     message.respond({
-      event: "close",
-      closeCode: code
+      event: 'close',
+      closeCode: code,
     });
   });
-  proc.on('exit', function (code) {
+  proc.on('exit', (code) => {
     message.respond({
-      event: "exit",
-      exitCode: code
+      event: 'exit',
+      exitCode: code,
     });
   });
 });
 
 // stub service that emulates luna://com.webos.service.sm/license/apps/getDrmStatus
-service.register("getDrmStatus", (message) => {
+service.register('getDrmStatus', (message) => {
   message.respond({
-    "appId": message.payload.appId,
-    "drmType": "NCG DRM",
-    "installBasePath": "/media/cryptofs",
-    "returnValue": true,
-    "isTimeLimited": false
+    appId: message.payload.appId,
+    drmType: 'NCG DRM',
+    installBasePath: '/media/cryptofs',
+    returnValue: true,
+    isTimeLimited: false,
   });
 });
