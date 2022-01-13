1b2540b4578c5352bcaba9e913db0bf0c58d5d84
Piotr Dobrowolski <Informatic@users.noreply.github.com>
2022-01-13 16:32:14 +0100

    elevate-service: implement patching of native services and their roles (#72)
    * elevate-service: implement patching of native services and their roles
    
    * elevate-service: add support for webOS 4.x allowedNames
    
    * elevate-service: add all services permission...
    
    * elevate-service: add support for multiple required names
    
    * elevate-service: fix api-permissions.d and native services on 3.x
    
    Due to a typo, elevate-service released in 0.3.2 was putting api
    permissions file in client-permissions.d directory. Since we were
    already patching manifest files with a proper path, in practice, this
    didn't affect webOS 4.x+, and on webOS 3.x all services are exposed
    publicly anyway.
    
    Configuration files in client-permissions.d are compatible schema-wise
    with the ones in api-permissions.d, and shouldn't affect our configs,
    outside of some spurious files. These probably should be cleaned up at
    some point.
    
    Additionally, we now handle the case where native services don't have
    /var/luna-service2-dev/services.d entry but still need some config
    patching in /var/luna-service2-dev by looking for existence of parent
    directories of files we touch.
    
    Now we also properly patch legacy pub role file, which was missed as
    well.

--- services/elevate-service.ts
+++ services/elevate-service.ts
@@ -2,6 +2,7 @@
 
 import { statSync, readFileSync, writeFileSync } from 'fs';
 import { execFile } from 'child_process';
+import { dirname } from 'path';
 
 process.env.PATH = `/usr/sbin:${process.env.PATH}`;
 
@@ -13,9 +14,31 @@ function isFile(path: string): boolean {
   }
 }
 
+function parentExists(path: string): boolean {
+  try {
+    return statSync(dirname(path)).isDirectory();
+  } catch (err) {
+    return false;
+  }
+}
+
 function patchServiceFile(serviceFile: string): boolean {
   const serviceFileOriginal = readFileSync(serviceFile).toString();
-  const serviceFileNew = serviceFileOriginal.replace('/usr/bin', '/media/developer/apps/usr/palm/services/org.webosbrew.hbchannel.service');
+  let serviceFileNew = serviceFileOriginal;
+
+  if (serviceFileNew.indexOf('/run-js-service') !== -1) {
+    console.info(`[ ] ${serviceFile} is a JS service`);
+    serviceFileNew = serviceFileNew.replace(
+      /^Exec=\/usr\/bin\/run-js-service/gm,
+      'Exec=/media/developer/apps/usr/palm/services/org.webosbrew.hbchannel.service/run-js-service',
+    );
+  } else if (serviceFileNew.indexOf('/jailer') !== -1) {
+    console.info(`[ ] ${serviceFile} is a native service`);
+    serviceFileNew = serviceFileNew.replace(/^Exec=\/usr\/bin\/jailer .* ([^ ]*)$/gm, (match, binaryPath) => `Exec=${binaryPath}`);
+  } else if (serviceFileNew.indexOf('Exec=/media') === -1) {
+    // Ignore elevated native services...
+    console.info(`[~] ${serviceFile}: unknown service type, this may cause some troubles`);
+  }
 
   if (serviceFileNew !== serviceFileOriginal) {
     console.info(`[ ] Updating service definition: ${serviceFile}`);
@@ -24,6 +47,63 @@ function patchServiceFile(serviceFile: string): boolean {
     writeFileSync(serviceFile, serviceFileNew);
     return true;
   }
+  return false;
+}
+
+function patchRolesFile(path: string, requiredNames: string[] = ['*', 'com.webos.service.capture.client*']) {
+  const rolesOriginal = readFileSync(path).toString();
+  const rolesNew = JSON.parse(rolesOriginal);
+
+  for (const name of requiredNames) {
+    // webOS <4.x /var/ls2-dev role file
+    if (rolesNew?.role?.allowedNames && rolesNew?.role?.allowedNames.indexOf(name) === -1) {
+      rolesNew.role.allowedNames.push(name);
+    }
+
+    // webOS 4.x+ /var/luna-service2 role file
+    if (rolesNew?.allowedNames && rolesNew?.allowedNames.indexOf(name) === -1) {
+      rolesNew.allowedNames.push(name);
+    }
+  }
+
+  // permissions / allowedNames interactions are fairly odd. It seems like
+  // "service" field in permission is one of allowedNames that this executable
+  // can use, outbound are remote client names that our executable/name can use,
+  // and inbound are remote client names that can access our executable.
+  //
+  // Oddly, even though there seems to be some support for wildcards, some
+  // pieces of software verify explicit permission "service" key, thus we
+  // sometimes may need some extra allowedNames/permissions, even though we
+  // default to "*"
+  if (rolesNew.permissions) {
+    const missingPermissionNames = requiredNames;
+    rolesNew.permissions.forEach((perm: { outbound?: string[]; service?: string }) => {
+      if (perm.service && missingPermissionNames.indexOf(perm.service) !== -1)
+        missingPermissionNames.splice(missingPermissionNames.indexOf(perm.service), 1);
+      if (perm.outbound && perm.outbound.indexOf('*') === -1) {
+        perm.outbound.push('*');
+      }
+    });
+
+    for (const name of missingPermissionNames) {
+      console.info(`[ ] Adding permission for name: ${name}`);
+      rolesNew.permissions.push({
+        service: name,
+        inbound: ['*'],
+        outbound: ['*'],
+      });
+    }
+  }
+
+  const rolesNewContents = JSON.stringify(rolesNew);
+  if (rolesNewContents !== JSON.stringify(JSON.parse(rolesOriginal))) {
+    console.info(`[ ] Updating roles definition: ${path}`);
+    console.info('-', rolesOriginal);
+    console.info('+', rolesNewContents);
+    writeFileSync(path, rolesNewContents);
+    return true;
+  }
+
   return false;
 }
 
@@ -39,64 +119,72 @@ function main(argv: string[]) {
 
   const serviceFile = `/var/luna-service2-dev/services.d/${serviceName}.service`;
   const clientPermFile = `/var/luna-service2-dev/client-permissions.d/${serviceName}.root.json`;
-  const apiPermFile = `/var/luna-service2-dev/client-permissions.d/${serviceName}.api.public.json`;
+  const apiPermFile = `/var/luna-service2-dev/api-permissions.d/${serviceName}.api.public.json`;
   const manifestFile = `/var/luna-service2-dev/manifests.d/${appName}.json`;
+  const roleFile = `/var/luna-service2-dev/roles.d/${serviceName}.service.json`;
 
   if (isFile(serviceFile)) {
     console.info(`[~] Found webOS 3.x+ service file: ${serviceFile}`);
     if (patchServiceFile(serviceFile)) {
       configChanged = true;
     }
+  }
 
-    if (!isFile(clientPermFile)) {
-      console.info(`[ ] Creating client permissions file: ${clientPermFile}`);
-      writeFileSync(
-        clientPermFile,
-        JSON.stringify({
-          [`${serviceName}*`]: ['all'],
-        }),
-      );
+  if (parentExists(clientPermFile) && !isFile(clientPermFile)) {
+    console.info(`[ ] Creating client permissions file: ${clientPermFile}`);
+    writeFileSync(
+      clientPermFile,
+      JSON.stringify({
+        [`${serviceName}*`]: ['all'],
+      }),
+    );
+    configChanged = true;
+  }
+
+  if (parentExists(apiPermFile) && !isFile(apiPermFile)) {
+    console.info(`[ ] Creating API permissions file: ${apiPermFile}`);
+    writeFileSync(
+      apiPermFile,
+      JSON.stringify({
+        public: [`${serviceName}/*`],
+      }),
+    );
+    configChanged = true;
+  }
+
+  if (isFile(roleFile)) {
+    if (patchRolesFile(roleFile)) {
       configChanged = true;
     }
+  }
 
-    if (!isFile(apiPermFile)) {
-      console.info(`[ ] Creating API permissions file: ${apiPermFile}`);
-      writeFileSync(
-        apiPermFile,
-        JSON.stringify({
-          public: [`${serviceName}/*`],
-        }),
-      );
-      configChanged = true;
+  if (isFile(manifestFile)) {
+    console.info(`[~] Found webOS 4.x+ manifest file: ${manifestFile}`);
+    const manifestFileOriginal = readFileSync(manifestFile).toString();
+    const manifestFileParsed = JSON.parse(manifestFileOriginal);
+    if (manifestFileParsed.clientPermissionFiles && manifestFileParsed.clientPermissionFiles.indexOf(clientPermFile) === -1) {
+      console.info('[ ] manifest - adding client permissions file...');
+      manifestFileParsed.clientPermissionFiles.push(clientPermFile);
     }
 
-    if (isFile(manifestFile)) {
-      console.info(`[~] Found webOS 4.x+ manifest file: ${manifestFile}`);
-      const manifestFileOriginal = readFileSync(manifestFile).toString();
-      const manifestFileParsed = JSON.parse(manifestFileOriginal);
-      if (manifestFileParsed.clientPermissionFiles && manifestFileParsed.clientPermissionFiles.indexOf(clientPermFile) === -1) {
-        console.info('[ ] manifest - adding client permissions file...');
-        manifestFileParsed.clientPermissionFiles.push(clientPermFile);
-      }
+    if (manifestFileParsed.apiPermissionFiles && manifestFileParsed.apiPermissionFiles.indexOf(apiPermFile) === -1) {
+      console.info('[ ] manifest - adding API permissions file...');
+      manifestFileParsed.apiPermissionFiles.push(apiPermFile);
+    }
 
-      if (manifestFileParsed.apiPermissionFiles && manifestFileParsed.apiPermissionFiles.indexOf(apiPermFile) === -1) {
-        console.info('[ ] manifest - adding API permissions file...');
-        manifestFileParsed.apiPermissionFiles.push(apiPermFile);
-      }
-
-      const manifestFileNew = JSON.stringify(manifestFileParsed);
-      if (manifestFileNew !== manifestFileOriginal) {
-        console.info(`[~] Updating manifest file: ${manifestFile}`);
-        console.info('-', manifestFileOriginal);
-        console.info('+', manifestFileNew);
-        writeFileSync(manifestFile, manifestFileNew);
-        configChanged = true;
-      }
+    const manifestFileNew = JSON.stringify(manifestFileParsed);
+    if (manifestFileNew !== manifestFileOriginal) {
+      console.info(`[~] Updating manifest file: ${manifestFile}`);
+      console.info('-', manifestFileOriginal);
+      console.info('+', manifestFileNew);
+      writeFileSync(manifestFile, manifestFileNew);
+      configChanged = true;
     }
   }
 
   const legacyPubServiceFile = `/var/palm/ls2-dev/services/pub/${serviceName}.service`;
-  const legacyPrvServiceFile = `/var/palm/ls2-dev/services/pub/${serviceName}.service`;
+  const legacyPrvServiceFile = `/var/palm/ls2-dev/services/prv/${serviceName}.service`;
+  const legacyPubRolesFile = `/var/palm/ls2-dev/roles/pub/${serviceName}.json`;
   const legacyPrvRolesFile = `/var/palm/ls2-dev/roles/prv/${serviceName}.json`;
 
   if (isFile(legacyPubServiceFile)) {
@@ -108,23 +196,23 @@ function main(argv: string[]) {
     if (patchServiceFile(legacyPrvServiceFile)) {
       configChanged = true;
     }
+  }
 
-    if (isFile(legacyPrvRolesFile)) {
-      const prvRolesOriginal = readFileSync(legacyPrvRolesFile).toString();
-      const prvRolesNew = prvRolesOriginal.replace('"outbound":[]', '"outbound":["*"]');
-      if (prvRolesNew !== prvRolesOriginal) {
-        console.info(`[ ] Updating service definition: ${legacyPrvRolesFile}`);
-        console.info('-', prvRolesOriginal);
-        console.info('+', prvRolesNew);
-        writeFileSync(legacyPrvRolesFile, prvRolesNew);
-        configChanged = true;
-      }
+  if (isFile(legacyPubRolesFile)) {
+    if (patchRolesFile(legacyPubRolesFile)) {
+      configChanged = true;
+    }
+  }
+
+  if (isFile(legacyPrvRolesFile)) {
+    if (patchRolesFile(legacyPrvRolesFile)) {
+      configChanged = true;
     }
   }
 
   if (configChanged) {
     console.info('[+] Refreshing services...');
-    execFile('ls-control', ['scan-services'], (err, stderr, stdout) => {
+    execFile('ls-control', ['scan-services'], { timeout: 10000 }, (err, stderr, stdout) => {
       if (err) {
         console.error(err);
         process.exit(1);
