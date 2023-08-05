981da350701ac498ca799b2cf8219af27425317f
kitsuned <80879390+kitsuned@users.noreply.github.com>
2023-08-06 03:11:03 +0500

    let's test some modern ES features!

--- frontend/baseurl.js
+++ frontend/baseurl.js
@@ -1,7 +1,8 @@
 // https://www.ietf.org/rfc/rfc3986.txt - page 50
-function parseURL(url) {
-  var re = new RegExp("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?");
-  var res = url.match(re);
+export function parseURL(url) {
+  const re = new RegExp('^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?');
+  const res = url.match(re);
+
   return {
     scheme: res[2],
     authority: res[4],
@@ -15,21 +16,21 @@ function parseURL(url) {
  * Resolve URL url based off URL base (equivalent API to nodejs url.resolve with
  * arguments flipped)
  */
-function resolveURL(url, base) {
-  var p1 = parseURL(url);
-  var p2 = parseURL(base);
+export function resolveURL(url, base) {
+  const p1 = parseURL(url);
+  const p2 = parseURL(base);
 
   if (p1.scheme) return url;
 
-  if (p1.path.indexOf('/') == 0) {
-    return p2.scheme + '://' + p2.authority + p1.path + (p1.query ? '?' + p1.query : '');
+  if (p1.path.startsWith('/')) {
+    return `${p2.scheme}://${p2.authority}${p1.path}${p1.query ? `?${p1.query}` : ''}`;
   } else {
-    var b = (p2.path.lastIndexOf('/') != -1) ? p2.path.substring(0, p2.path.lastIndexOf('/')) : p2.path;
-    return p2.scheme + '://' + p2.authority + b + '/' + p1.path + (p1.query ? '?' + p1.query : '');
+    const b = p2.path.lastIndexOf('/') != -1 ? p2.path.substring(0, p2.path.lastIndexOf('/')) : p2.path;
+    return `${p2.scheme}://${p2.authority}${b}/${p1.path}${p1.query ? '?' + p1.query : ''}`;
   }
 }
 
-function test() {
+export function test() {
   // Some quick testcases...
   var testCases = [
     ['test', 'https://google.com', 'https://google.com/test'],
@@ -55,9 +56,3 @@ function test() {
     console.info(t[0], t[1], res, res == t[2]);
   });
 }
-
-module.exports = {
-  parseURL: parseURL,
-  resolveURL: resolveURL,
-  test: test,
-};
--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,4 @@
-/**
-	Instantiate your enyo/Application kind in this file.  Note, application
-	rendering should be deferred until the DOM is ready by wrapping it in a
-	call to ready().
-*/
+import ready from 'enyo/ready';
+import App from './App';
 
-var
-  ready = require('enyo/ready'),
-  App = require('./App');
-
-ready(function () {
-  new App();
-});
+ready(() => new App());
